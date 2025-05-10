d3ffb91e07ba06cec585d85ff40ef268c1c90d49
akarahdev <endisticbutitsforgit@gmail.com>
2025-05-10 10:55:39 -0400

    build: initialize repository

---
+++ codetemplates/src/main/java/dev/akarah/codetemplate/Main.java
@@ -0,0 +1,7 @@
+package dev.akarah.codetemplate;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "df-jvm"
+include("codetemplates")
